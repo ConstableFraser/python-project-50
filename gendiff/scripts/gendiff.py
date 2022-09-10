acf6b415fb45ec9abb7998508aecbae050bfb256
sanswed <sanswed@gmail.com>
2022-09-10 15:46:59 +0300

    .

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,6 @@
+import argparse
+parser =  argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+parser.add_argument("first_file", type=str)
+parser.add_argument("second_file", type=str)
+args = parser.parse_args()
+print(args)
