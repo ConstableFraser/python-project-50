3ffa3f59a8ab4d65e8bd2ad975abf450ad2ca716
sanswed <sanswed@gmail.com>
2022-09-16 23:15:55 +0300

    diff flat files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-### Hexlet tests and linter status:
+# DESCRIPTION
+# HOW TO INSTALL
+
+# Hexlet tests and linter status:
 [![Actions Status](https://github.com/ConstableFraser/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/ConstableFraser/python-project-50/actions)
+
+# ASCIINEMA RECORDS
+[![asciicast](https://asciinema.org/a/502029.svg)](https://asciinema.org/a/502029)
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,20 @@
 import argparse
-parser =  argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
-parser.add_argument("first_file", type=str)
-parser.add_argument("second_file", type=str)
-parser.add_argument("-f", "--format", type=str, help="set format of output")
-args = parser.parse_args()
-print(args)
+from gendiff.modul.to_string import to_string
+from gendiff.modul.get_diff import get_diff
+
+DESCRIPTION = "Compares two configuration files and shows a difference."
+HELP = "set format of output"
+
+
+def main():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument("-f", "--format", type=str, help=HELP)
+    args = vars(parser.parse_args())
+    diff = get_diff(args['first_file'], args['second_file'])
+    print(to_string(diff))
+
+
+if __name__ == "__main__":
+    main()
