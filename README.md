43137b5a0511a4709e33a6cce68e3033fa914cd8
sanswed <sanswed@gmail.com>
2022-09-18 12:02:37 +0300

    +bages and fix warnings

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# DESCRIPTION
-# HOW TO INSTALL
+## DESCRIPTION
+## HOW TO INSTALL
 
-# Hexlet tests and linter status:
+## Hexlet tests, linter status, code climate and test coverage:
 [![Actions Status](https://github.com/ConstableFraser/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/ConstableFraser/python-project-50/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/7212408bfd1a84de5cde/maintainability)](https://codeclimate.com/github/ConstableFraser/python-project-50/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/7212408bfd1a84de5cde/test_coverage)](https://codeclimate.com/github/ConstableFraser/python-project-50/test_coverage)
 
-# ASCIINEMA RECORDS
+## ASCIINEMA RECORDS
 [![asciicast](https://asciinema.org/a/521850.svg)](https://asciinema.org/a/521850)
--- gendiff/modul/get_diff.py
+++ gendiff/modul/get_diff.py
@@ -2,20 +2,24 @@ import json
 
 
 def get_diff(file1, file2):
-    dict1 = json.load(open(file1))
-    dict2 = json.load(open(file2))
+    dct1 = json.load(open(file1))
+    dct2 = json.load(open(file2))
     result = []
-    keys_set = set(tuple(dict1.keys()) + tuple(dict2.keys()))
+    keys_set = set(tuple(dct1.keys()) + tuple(dct2.keys()))
+    keys_set = sorted(keys_set)
+
+    def matching(item):
+        if (item in dct1) and (item in dct2) and (dct1[item] == dct2[item]):
+            return [[' ', item, dct1[item]]]
+        elif (item in dct1) and (item in dct2) and (dct1[item] != dct2[item]):
+            return [['-', item, dct1[item]], ['+', item, dct2[item]]]
+        if (item in dct1):
+            return [['-', item, dct1[item]]]
+        else:
+            return [['+', item, dct2[item]]]
+    result = []
+    keys_set = set(tuple(dct1.keys()) + tuple(dct2.keys()))
     keys_set = sorted(keys_set)
     for item in keys_set:
-        if (item in dict1) and (item in dict2):
-            if dict1[item] == dict2[item]:
-                result.extend([[' ', item, dict1[item]]])
-            else:
-                result.extend([['-', item, dict1[item]]])
-                result.extend([['+', item, dict2[item]]])
-        elif (item in dict1) is False:
-            result.extend([['+', item, dict2[item]]])
-        elif (item in dict2) is False:
-            result.extend([['-', item, dict1[item]]])
+        result.extend(matching(item))
     return result
