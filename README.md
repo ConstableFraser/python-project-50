433da6d6609fed78ca246373ec40c91b4c646e56
sanswed <sanswed@gmail.com>
2022-09-18 12:17:13 +0300

    add linter badge

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 [![Actions Status](https://github.com/ConstableFraser/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/ConstableFraser/python-project-50/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7212408bfd1a84de5cde/maintainability)](https://codeclimate.com/github/ConstableFraser/python-project-50/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/7212408bfd1a84de5cde/test_coverage)](https://codeclimate.com/github/ConstableFraser/python-project-50/test_coverage)
+[![Linter](https://github.com/ConstableFraser/python-project-50/actions/workflows/Linter.yml/badge.svg)](https://github.com/ConstableFraser/python-project-50/actions/workflows/Linter.yml)
 
 ## ASCIINEMA RECORDS
 [![asciicast](https://asciinema.org/a/521850.svg)](https://asciinema.org/a/521850)
--- gendiff/modul/get_diff.py
+++ gendiff/modul/get_diff.py
@@ -7,19 +7,15 @@ def get_diff(file1, file2):
     result = []
     keys_set = set(tuple(dct1.keys()) + tuple(dct2.keys()))
     keys_set = sorted(keys_set)
-
-    def matching(item):
-        if (item in dct1) and (item in dct2) and (dct1[item] == dct2[item]):
-            return [[' ', item, dct1[item]]]
-        elif (item in dct1) and (item in dct2) and (dct1[item] != dct2[item]):
-            return [['-', item, dct1[item]], ['+', item, dct2[item]]]
-        if (item in dct1):
-            return [['-', item, dct1[item]]]
-        else:
-            return [['+', item, dct2[item]]]
-    result = []
-    keys_set = set(tuple(dct1.keys()) + tuple(dct2.keys()))
-    keys_set = sorted(keys_set)
     for item in keys_set:
-        result.extend(matching(item))
+        if (item in dct1) and (item in dct2) and (dct1[item] == dct2[item]):
+            result.extend([[' ', item, dct1[item]]])
+            continue
+        elif (item in dct1) and (item in dct2) and (dct1[item] != dct2[item]):
+            result.extend([['-', item, dct1[item]], ['+', item, dct2[item]]])
+            continue
+        if (item in dct1):
+            result.extend([['-', item, dct1[item]]])
+        else:
+            result.extend([['+', item, dct2[item]]])
     return result
