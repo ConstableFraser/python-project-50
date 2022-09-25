d9b1ef2c08c9036a61381d6e9c7b3ca36a9f5bad
sanswed <sanswed@gmail.com>
2022-09-25 11:53:27 +0300

    +new test; *fix bugs

--- tests/template/test_template.py
+++ tests/template/test_template.py
@@ -3,7 +3,7 @@ import json
 from gendiff.generate_diff import generate_diff
 
 @pytest.fixture
-def files():
+def files1():
     return ["tests/fixtures/template/template1.json",
             "tests/fixtures/template/template2.json",
             "tests/fixtures/template/result.txt"
@@ -15,9 +15,17 @@ def files2():
             "tests/fixtures/template/template2.json",
            ]
 
+@pytest.fixture
+def files3():
+    return ["tests/fixtures/template/template2.json",
+            "tests/fixtures/template/template1.json",
+            "tests/fixtures/template/result2.txt"
+           ]
 
-def test_correct(files):
-    f1 , f2, result = files
+
+
+def test_correct(files1):
+    f1 , f2, result = files1
     f3 = open(result)
     result_list = f3.readlines()
     f3.close()
@@ -26,6 +34,18 @@ def test_correct(files):
         str_tmp += item
     assert str_tmp == generate_diff(f1, f2)
 
+
 def test_nosuchfile(files2):
     f1 , f2 = files2
     assert None == generate_diff(f1, f2)
+
+
+def test_correct(files3):
+    f1 , f2, result = files3
+    f3 = open(result)
+    result_list = f3.readlines()
+    f3.close()
+    str_tmp = ""
+    for item in result_list:
+        str_tmp += item
+    assert str_tmp == generate_diff(f1, f2)
