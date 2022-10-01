c8267c78091c4cb7582df643bf3267dfd15fa600
sanswed <sanswed@gmail.com>
2022-10-01 19:24:22 +0300

    *parser.py omptimized

--- gendiff/parser/parser.py
+++ gendiff/parser/parser.py
@@ -11,14 +11,12 @@ def parser(file1, file2):
 def load(file):
     if not os.path.isfile(file):
         return None
-    index = file.rfind(".")
-    if index < 0:
-        return None
-    ext = file[index + 1:].lower()
+    _, ext = os.path.splitext(file)
+    ext = ext.lower()
     f = open(file)
-    if ext == "json":
+    if ext == ".json":
         return json.load(f)
-    elif (ext == "yaml" or ext == "yml"):
+    elif (ext == ".yaml" or ext == ".yml"):
         return yaml.safe_load(f)
     else:
         return None
