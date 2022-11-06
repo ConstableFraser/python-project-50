f4f7f0cd197b7d6671b692acf751d36f51b1e98f
sanswed <sanswed@gmail.com>
2022-11-06 16:23:15 +0300

    step#8 v.1.4 fix hexlet-check

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,3 @@
-from gendiff import generate_diff
+#from gendiff import generate_diff
 
-__all__ = ['generate_diff']
+#__all__ = ['generate_diff']
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff.generate_diff import generate_diff
 
 def main():
     args = parse_args()
-    diff = generate_diff(args["frst_file"], args["scnd_file"], args["format"])
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
